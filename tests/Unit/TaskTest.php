dc92348bf80bc862bcf811f35414d63be2f94a94
taeshinnn <60128519+taeshinnn@users.noreply.github.com>
2022-03-23 11:30:05 +0700

    schema task test

--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -10,13 +10,46 @@ class TaskTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
 
-    /** @test  */
-    public function Tasks_database_has_expected_columns()
+    // Task Schema Test 
+    public function tasks_database_has_expected_columns()
     {
         $this->assertTrue( 
           Schema::hasColumns('tasks', [
             'id','description', 'user_id', 'created_at', 'updated_at'
         ]), 1);
+    }
+
+    public function test_database_has_user_id_columns()
+    {
+        $this->assertTrue(
+            Schema::hasColumn(
+                'tasks',
+                'user_id'
+            ),
+            1
+        );
+    }
+
+    public function test_database_has_description_columns()
+    {
+        $this->assertTrue(
+            Schema::hasColumn(
+                'tasks',
+                'description'
+            ),
+            1
+        );
+    }
+
+    public function test_database_has_id_columns()
+    {
+        $this->assertTrue(
+            Schema::hasColumn(
+                'tasks',
+                'id'
+            ),
+            1
+        );
     }
 
     // public function testDatabase()
