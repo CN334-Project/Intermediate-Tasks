350249c0975c49caa44e945275568e50fef85bbc
Mansea <6210742034@student.tu.ac.th>
2022-03-23 11:35:54 +0700

    Update UserTest.php

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -10,13 +10,31 @@ class UserTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
 
-    /** @test  */
-    // test
+    //Schema Test all columns
     public function users_database_has_expected_columns()
     {
-        $this->assertTrue( 
+        $this->assertTrue(
           Schema::hasColumns('users', [
             'id','name', 'email', 'email_verified_at', 'password'
         ]), 1);
     }
+
+    public function stupidfuck()
+    {
+        $this->assertTrue(
+            Schema::hasColumn('users', 'name'),1
+        );
+    }
+
+    public function kuy()
+    {
+        $this->assertTrue(false);
+    }
+
+    // public function users_database_has_email_column()
+    // {
+    //     $this->assertTrue(
+    //         Schema::hasColumn('users', 'email'),1
+    //     );
+    // }
 }
