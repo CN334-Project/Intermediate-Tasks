aa5233828613f79f627f697b71254803aa250442
P2OM <heammapoom@gmail.com>
2022-04-20 13:27:22 +0700

    RegisterBAT

--- tests/BAT/RegisterBAT.php
+++ tests/BAT/RegisterBAT.php
@@ -12,40 +12,140 @@ class RegisterBAT extends TestCase {
 
     use WithFaker, RefreshDatabase;
 
+    //Register with non-name input
+    public function test_register_with_non_name_input()
+    {
+        $user = [
+            'name' => '',
+            'email' => 'testemail@test.com',
+            'password' => 'passwordtest',
+            'password_confirmation' => 'passwordtest'
+        ];
+
+        $response = $this->post('/register', $user);
+
+        $this->assertDatabaseMissing('users', ['name' => '', 'email' => 'testemail@test.com']);
+    }
+
     //Register with non-email input
     public function test_register_with_non_email_input()
     {
-        User::factory()->create([
+        $user = [
+            'name' => 'Test',
             'email' => '',
-        ]);
+            'password' => 'passwordtest',
+            'password_confirmation' => 'passwordtest'
+        ];
 
-        $this->assertDatabaseHas('users', ['email' => '']);
+        $response = $this->post('/register', $user);
+
+        $this->assertDatabaseMissing('users', ['name' => 'Test', 'email' => '']);
     }
 
     //Register with non-password input
     public function test_register_with_non_password_input()
     {
-        User::factory()->create([
+        $user = [
+            'name' => 'Test',
+            'email' => 'testemail@test.com',
             'password' => '',
-        ]);
+            'password_confirmation' => ''
+        ];
 
-        $this->assertDatabaseHas('users', ['password' => '']);
+        $response = $this->post('/register', $user);
+
+        $this->assertDatabaseMissing('users', ['name' => 'Test', 'email' => 'testemail@test.com']);
     }
 
-    //Register with non-name input
+    //Register with valid name input
+    public function test_register_with_valid_name_input()
+    {
+        $user = [
+            'name' => 'Test',
+            'email' => 'testemail@test.com',
+            'password' => 'passwordtest',
+            'password_confirmation' => 'passwordtest'
+        ];
+
+        $response = $this->post('/register', $user);
+
+        $this->assertDatabaseHas('users', ['name' => 'Test', 'email' => 'testemail@test.com']);
+    }
 
     //Register with valid email input
+    public function test_register_with_valid_email_input()
+    {
+        $user = [
+            'name' => 'Test',
+            'email' => 'testemail@test.com',
+            'password' => 'passwordtest',
+            'password_confirmation' => 'passwordtest'
+        ];
+
+        $response = $this->post('/register', $user);
+
+        $this->assertDatabaseHas('users', ['name' => 'Test', 'email' => 'testemail@test.com']);
+    }
 
     //Register with valid password input
+    public function test_register_with_valid_password_input()
+    {
+        $user = [
+            'name' => 'Test',
+            'email' => 'testemail@test.com',
+            'password' => 'passwordtest',
+            'password_confirmation' => 'passwordtest'
+        ];
 
-    //Register with valid name input
+        $response = $this->post('/register', $user);
 
-    //Register with invalid email input
-
-    //Register with invalid password input
+        $this->assertDatabaseHas('users', ['name' => 'Test', 'email' => 'testemail@test.com']);
+    }
 
     //Register with invalid name input
+    public function test_register_with_invalid_name_input()
+    {
+        $user = [
+            'name' => '',
+            'email' => 'testemail@test.com',
+            'password' => 'passwordtest',
+            'password_confirmation' => 'passwordtest'
+        ];
 
+        $response = $this->post('/register', $user);
+
+        $this->assertDatabaseMissing('users', ['name' => '', 'email' => 'testemail@test.com']);
+    }
+
+    //Register with invalid email input
+    public function test_register_with_invalid_email_input()
+    {
+        $user = [
+            'name' => 'Test',
+            'email' => 'wrong-email',
+            'password' => 'passwordtest',
+            'password_confirmation' => 'passwordtest'
+        ];
+
+        $response = $this->post('/register', $user);
+
+        $this->assertDatabaseMissing('users', ['name' => 'Test', 'email' => 'wrong-email']);
+    }
+
+    //Register with invalid password input
+    public function test_register_with_invalid_password_input()
+    {
+        $user = [
+            'name' => 'Test',
+            'email' => 'testemail@test.com',
+            'password' => '123',
+            'password_confirmation' => '123'
+        ];
+
+        $response = $this->post('/register', $user);
+
+        $this->assertDatabaseMissing('users', ['name' => 'Test', 'email' => 'testemail@test.com']);
+    }
 
 }
 ?>
