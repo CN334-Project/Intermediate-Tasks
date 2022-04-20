4f4397cb1434de99ce521ee0d0f445428df84b8b
taeshinnn <60128519+taeshinnn@users.noreply.github.com>
2022-04-20 12:42:35 +0700

    Login BAT

--- tests/BAT/LoginBAT.php
+++ tests/BAT/LoginBAT.php
@@ -1,12 +1,44 @@
 <?php
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 class LoginBAT extends TestCase
 {
-    public function test_assert()
+
+    use WithFaker, RefreshDatabase;
+
+    //Login
+    public function test_login_with_valid_credentials()
     {
-        $this->assertTrue(true);
+
+        $user = User::factory()->create([
+            'email' => '',
+        ]);
+
+        $this->post('/login', [
+            'email' => $user->email,
+            'password' => $user->password,
+            'name' => $user->name
+        ]);
+
+        $this->assertGuest();
+    }
+
+    public function test_login_with_valid_incredentials()
+    {
+        $user = User::factory()->create();
+
+        $this->post('/login', [
+            'email' => $user->email,
+            'password' => $user->password,
+            'name' => $user->name
+        ]);
+
+        $this->assertGuest();
     }
 
 }
