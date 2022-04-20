905a5ea48251fcf763e16a5b0372b8ee3e9a83b3
taeshinnn <60128519+taeshinnn@users.noreply.github.com>
2022-04-20 13:02:24 +0700

    Login BAT Revision I

--- tests/BAT/LoginBAT.php
+++ tests/BAT/LoginBAT.php
@@ -3,6 +3,7 @@
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use App\Models\User;
 
@@ -12,7 +13,7 @@ class LoginBAT extends TestCase
     use WithFaker, RefreshDatabase;
 
     //Login
-    public function test_login_with_valid_credentials()
+    public function test_login_with_invalid_credentials()
     {
 
         $user = User::factory()->create([
@@ -28,14 +29,26 @@ class LoginBAT extends TestCase
         $this->assertGuest();
     }
 
-    public function test_login_with_valid_incredentials()
+    public function test_users_can_authenticate_using_the_login_screen()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+        $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+
+    public function test_users_can_not_authenticate_with_invalid_password()
     {
         $user = User::factory()->create();
 
         $this->post('/login', [
             'email' => $user->email,
-            'password' => $user->password,
-            'name' => $user->name
+            'password' => 'wrong-password',
         ]);
 
         $this->assertGuest();
