f0ac75f41a6e071f4cc74c473bd7408a9205d61a
taeshinnn <60128519+taeshinnn@users.noreply.github.com>
2022-04-20 12:02:47 +0700

    Edit & Delete Task BAT

--- tests/BAT/DeleteTaskBAT.php
+++ tests/BAT/DeleteTaskBAT.php
@@ -1,9 +1,18 @@
 <?php
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Task;
 
 class DeleteTaskBAT extends TestCase {
 
-
+    public function test_delete_task_in_database()
+    {
+        $task = Task::factory()->create();
+        $task -> delete();
+        $this->assertModelMissing($task);
+    }
 }
 ?>
--- tests/BAT/EditTaskBAT.php
+++ tests/BAT/EditTaskBAT.php
@@ -1,9 +1,63 @@
 <?php
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Task;
 
 class EditTaskBAT extends TestCase {
 
+    public function test_edit_description_with_valid_input()
+    {
+        $task = Task::factory()->create([
+            'description' => 'test desc',
+        ]);
 
+        $task -> edit([
+            'description' => 'Hello world',
+        ]);
+
+        $this->assertDatabaseHas('tasks', ['description' => 'Hello world']);
+    }
+
+    public function test_edit_description_with_invalid_input()
+    {
+        $task = Task::factory()->create([
+            'description' => 'test desc',
+        ]);
+
+        $task -> edit([
+            'description' => '你好世界 操你妈死',
+        ]);
+
+        $this->assertDatabaseHas('tasks', ['description' => '你好世界 操你妈死']);
+    }
+
+    public function test_edit_description_with_whitespace_input()
+    {
+        $task = Task::factory()->create([
+            'description' => 'test desc',
+        ]);
+
+        $task -> edit([
+            'description' => '',
+        ]);
+
+        $this->assertDatabaseHas('tasks', ['description' => '']);
+    }
+
+    public function test_edit_description_with_large_input()
+    {
+        $task = Task::factory()->create([
+            'description' => 'test desc',
+        ]);
+
+        $task -> edit([
+            'description' => 'asdoifjapsodihgfpiasudhfgpouashdfpouahsdpifuhaspidufhapisduhfpiasuhdfpiasuhdfpiaushdpfiuhasdipufhapisudhfpiausdhfpiauhsdpfiuhasdpiufghaspdughfpaisudhfgpiaushdgfpiu',
+        ]);
+
+        $this->assertDatabaseHas('tasks', ['description' => '']);
+    }
 }
 ?>
